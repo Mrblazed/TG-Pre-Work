61ac6822d53973f63d60b1c345513b20779a88f6
Adam Disanza <adamdisanza@gmail.com>
2018-12-24 10:38:24 -0500

    Exploring the use of if else statments

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/if-else-Statements.js
@@ -0,0 +1,12 @@
+let sale = true;
+
+sale = false;
+
+if(sale) {
+  console.log('Time to buy!');}
+else {
+  console.log('Time to wait for a sale.')
+}
+
+
+// Exploring the use of if else statments
