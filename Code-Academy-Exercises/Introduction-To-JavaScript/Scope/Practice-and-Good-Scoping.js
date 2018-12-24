a99c3276292a3b2f4f70ae90834d728e6ae74be2
Adam Disanza <adamdisanza@gmail.com>
2018-12-24 14:57:08 -0500

    Worked on what would be the proper scope for variables

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Scope/Practice-and-Good-Scoping.js
@@ -0,0 +1,14 @@
+const logVisibleLightWaves = () => {
+  let lightWaves = 'Moonlight';
+	let region = 'The Arctic';
+  // Add if statement here:
+  if(region === 'The Arctic'){
+    let lightWaves = 'Northern Lights';
+    console.log(lightWaves);
+  }
+  console.log(lightWaves);
+};
+
+logVisibleLightWaves();
+
+//  Worked on what would be the proper scope for variables
