b0b6ab354395456d46005a302e73562f9d18e977
Adam Disanza <adamdisanza@gmail.com>
2018-12-24 10:22:10 -0500

    Used string interpolation to dynamically add variables to console.log outputs

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/String-Interpolation.js
@@ -0,0 +1,5 @@
+var myName = 'Adamn';
+var myCity = 'Limington';
+console.log(`My name is ${myName}!  My favorite city is ${myCity}.`);
+
+//  Used string interpolation to dynamically add variables to console.log outputs
