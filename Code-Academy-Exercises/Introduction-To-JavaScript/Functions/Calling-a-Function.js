dfc683bc1c65033aba9071735afd04aa4d323467
Adam Disanza <adamdisanza@gmail.com>
2018-12-24 11:42:12 -0500

    Created a function and called it three times

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Calling-a-Function.js
@@ -0,0 +1,9 @@
+function sayThanks() {
+  console.log('Thank you for your purchase! We appreciate your business.');
+}
+
+sayThanks()
+sayThanks()
+sayThanks()
+
+//  Created a function and called it three times
