e0dbd8dd91ea82e36b3e52ac3a5c4c8e664b15c3
Adam Disanza <adamdisanza@gmail.com>
2018-12-24 16:19:46 -0500

    Made an array, will be learing loops in upcoming lessons

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Repeating-Tasks-Manually.js
@@ -0,0 +1,10 @@
+// Write your code below
+const vacationSpots = ['London', 'France', 'Nantucket'];
+
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
+
+
+
+//  Made an array, will be learing loops in upcoming lessons
