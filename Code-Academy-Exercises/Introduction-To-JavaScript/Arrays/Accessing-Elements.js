d5c8a3f040727ecbc50b16b77fa6751663b14d2b
Adam Disanza <adamdisanza@gmail.com>
2018-12-24 15:12:57 -0500

    Accessed array information with a variable and console.log with brackets

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/Accessing-Elements.js
@@ -0,0 +1,7 @@
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+
+const listItem = famousSayings[0];
+console.log(listItem);
+console.log(famousSayings[2]);
+
+//  Accessed array information with a variable and console.log with brackets
