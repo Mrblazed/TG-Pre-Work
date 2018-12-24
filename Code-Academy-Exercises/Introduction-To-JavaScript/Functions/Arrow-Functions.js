a475bb25366d58808585233e4465356c256f04cd
Adam Disanza <adamdisanza@gmail.com>
2018-12-24 12:23:43 -0500

    Shorted code by replacing the word function with arrow function

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Arrow-Functions.js
@@ -0,0 +1,9 @@
+const plantNeedsWater = (day) => {
+  if (day === 'Wednesday') {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+//  Shorted code by replacing the word function with arrow function
