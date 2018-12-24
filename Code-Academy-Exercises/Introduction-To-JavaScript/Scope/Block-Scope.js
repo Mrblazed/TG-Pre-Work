24bb395041789099d5f38d9c6f16ebbeabe73d25
Adam Disanza <adamdisanza@gmail.com>
2018-12-24 14:26:47 -0500

    Experimented with using local scope vs global scope and trying to call const outside of scope

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Scope/Block-Scope.js
@@ -0,0 +1,9 @@
+function logVisibleLightWaves() {
+  const lightWaves = 'Moonlight'
+  console.log(lightWaves)
+}
+
+logVisibleLightWaves()
+console.log(lightWaves)
+
+//  Experimented with using local scope vs global scope and trying to call const outside of scope
