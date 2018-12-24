4184f60a76a970e5b34b31dff14f9f468fc8dc63
Adam Disanza <adamdisanza@gmail.com>
2018-12-24 11:59:53 -0500

    Used retuen to calculate variables of a function

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Return.js
@@ -0,0 +1,7 @@
+function monitorCount(rows, columns)
+{return rows * columns};
+const numOfMonitors = monitorCount(5,4);
+console.log(numOfMonitors);
+
+
+         //  Used retuen to calculate variables of a function
