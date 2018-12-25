f4861aabe0f3dd1a6ef6157610776ced256cd161
Adam Disanza <adamdisanza@gmail.com>
2018-12-25 09:06:40 -0500

    Used filter to create new array from existing array data

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Iterators/The-filter-Method.js
@@ -0,0 +1,22 @@
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+// Call .filter() on randomNumbers below
+
+const smallNumbers = randomNumbers.filter(small => small < 250);
+
+console.log(smallNumbers);
+
+const favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
+
+
+// Call .filter() on favoriteWords below
+
+const longFavortieWords = favoriteWords.filter(seven => {
+  return seven.length > 7;})
+console.log(longFavortieWords);
+
+
+
+
+
+//  Used filter to create new array from existing array data
