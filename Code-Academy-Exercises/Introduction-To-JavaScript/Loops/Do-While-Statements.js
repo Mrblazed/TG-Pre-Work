0b8520cc8bdf19628a7f78e685e178392cad943b
Adam Disanza <adamdisanza@gmail.com>
2018-12-25 06:40:02 -0500

    Created a do while loop

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Do-While-Statements.js
@@ -0,0 +1,13 @@
+// Write your code below
+// Write your code below
+
+
+
+const cupsOfSugarNeeded = 7;
+let cupsAdded = 0;
+ do {cupsAdded++}
+		while (cupsAdded < cupsOfSugarNeeded);
+
+ console.log(cupsAdded);
+
+ //  Created a do while loop
