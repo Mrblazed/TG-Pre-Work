c922abc4972c0da148d564e1a4e5be8abcd32112
Adam Disanza <adamdisanza@gmail.com>
2018-12-25 15:37:54 -0500

    Created a variable that grabbed Active Mission data from spaceship

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Bracket-Notation.js
@@ -0,0 +1,15 @@
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  'Active Mission' : true,
+  homePlanet : 'Earth',
+  numCrew: 5
+ };
+
+let propName =  'Active Mission';
+
+// Write your code below
+
+let isActive = spaceship['Active Mission'];
+console.log(isActive);
+
+//  Created a variable that grabbed Active Mission data from spaceship
