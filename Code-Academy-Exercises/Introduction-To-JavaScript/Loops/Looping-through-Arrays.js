0b590e5375cf3779f476acac21ac6fc0ba0dac17
Adam Disanza <adamdisanza@gmail.com>
2018-12-25 06:03:51 -0500

    Setup a loop that pulled data from an array

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Looping-through-Arrays.js
@@ -0,0 +1,9 @@
+const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+
+// Write your code below
+for (let i = 0; i < vacationSpots.length; i++){
+  console.log('I would love to visit'  + ' ' + vacationSpots[i]);
+
+};
+
+//  Setup a loop that pulled data from an array
