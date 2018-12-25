54a92753210a40ca74e49c025285512b1a8f90e4
Adam Disanza <adamdisanza@gmail.com>
2018-12-25 15:25:41 -0500

    Used dot modifiers to pull data from object

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Accessing-Properties.js
@@ -0,0 +1,15 @@
+let spaceship = {
+  homePlanet: 'Earth',
+  color: 'silver',
+  'Fuel Type': 'Turbo Fuel',
+  numCrew: 5,
+  flightPath: ['Venus', 'Mars', 'Saturn']
+};
+
+// Write your code below
+
+const crewCount = spaceship.numCrew;
+
+const planetArray = spaceship.flightPath;
+
+// Used dot modifiers to pull data from object
