10a20c5d175154b9f3e112eaae92182fa23c2f1e
Adam Disanza <adamdisanza@gmail.com>
2018-12-25 15:48:51 -0500

    Added a property and deleted a property from an object

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Property-Assignment.js
@@ -0,0 +1,16 @@
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth',
+  color: 'silver',
+  'Secret Mission' : 'Discover life outside of Earth.'
+};
+
+// Write your code below
+
+spaceship.color = 'glorious gold';
+
+spaceship.numEngines = 6;
+
+delete spaceship['Secret Mission'];
+
+//  Added a property and deleted a property from an object
