5057f5b5024961c7f8fd21387a41c6045e701d58
Adam Disanza <adamdisanza@gmail.com>
2018-12-26 00:31:55 -0500

    Changed arrow function to shorthand function to work with this method

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Advanced-Objects/Arrow-Functions-and-this.js
@@ -0,0 +1,10 @@
+const robot = {
+  energyLevel: 100,
+  checkEnergy(){
+    console.log(`Energy is currently at ${this.energyLevel}%.`)
+  }
+}
+
+robot.checkEnergy();
+
+// Changed arrow function to shorthand function to work with this method
