d38670b04787f252b21526161c49cd60735a9a2d
Adam Disanza <adamdisanza@gmail.com>
2018-12-26 04:16:49 -0500

    Looked up browser tech availability on caniuse.com

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/caniusedotcom-2.js
@@ -0,0 +1,7 @@
+// Set the variable below to a number
+let esFivePercentageSupport = 95.66;
+
+// Set the variable below to a number
+let esSixTemplateLiterals = 88.61;
+
+//  Looked up browser tech availability on caniuse.com
