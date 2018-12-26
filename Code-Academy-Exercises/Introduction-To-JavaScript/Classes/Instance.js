ebc2a1129badddb26c394f1de55ba34197bdce26
Adam Disanza <adamdisanza@gmail.com>
2018-12-26 02:39:00 -0500

    Created a class and made two new instances from that class

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Instance.js
@@ -0,0 +1,14 @@
+class Surgeon {
+  constructor(name, department) {
+    this.name = name;
+    this.department = department;
+  }
+}
+
+const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
+
+const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
+
+
+
+//  Created a class and made two new instances from that class
